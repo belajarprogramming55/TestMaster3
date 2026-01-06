cf6aad0b525c171b834c23a6c92f8103948037cd
OVAN <giovannimahendra55@gmail.com>
2026-01-06 12:34:16 +0700

    wow

--- Showroom2.cpp
+++ Showroom2.cpp
@@ -1,56 +1,56 @@
 #include <iostream>
-#include <iomanip>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    const int MAX_MOBIL = 100;
+    const int MAX_MOBIL = 3;
     string merek[MAX_MOBIL], model[MAX_MOBIL];
     int tahun[MAX_MOBIL];
     double harga[MAX_MOBIL];
     int jumlah = 0;
     int pilihan;
 
-    do{
-        cout << "\n=== SOHWROOM MOBIL ===\n";
-        cout << "1. Tambah Data Mobil\n";
-        cout << "2. Tampilkan Semua Data Mobil\n";
+    do {
+        cout << "\n=== Showroom Mobil ===\n";
+        cout << "1. Tambahkan mobil\n";
+        cout << "2. Tampilkan semua data mobil\n";
         cout << "3. Keluar dari showroom\n";
-        cout << "Pilih Menu (1-3): ";
+        cout << "Pilih Opsi (1-3): ";
         cin >> pilihan;
 
-        switch (pilihan) {
+        switch (pilihan){
             case 1:
             if (jumlah >= MAX_MOBIL) {
-                cout << "Data Mobil Sudah Penuh\n";
+                cout << "Showroom mobil sudah penuh\n";
             }else {
+                cout << "\n=== Tambahkan data mobil ===\n";
                 cout << "Merek Mobil: ";
                 cin >> merek[jumlah];
                 cout << "Model Mobil: ";
                 cin >> model[jumlah];
                 cout << "Tahun Mobil: ";
                 cin >> tahun[jumlah];
-                cout << "Harga Mobil; ";
+                cout << "Harga Mobil: ";
                 cin >> harga[jumlah];
 
                 jumlah++;
                 cout << "Data mobil berhasil ditambahkan\n";
-
             }
             break;
 
             case 2:
-            if (jumlah == 0) {
-                cout << "\nBelum ada data mobil.\n";
+            if (jumlah == 0){
+                cout << "Tidak ada data mobil di showroom.\n";
             }else {
-                cout << "\n=== Daftar Mobil di showroom ===\n";
+                cout << "\n=== Data Mobil di showroom ===\n";
                 cout << fixed << setprecision(0);
 
-                cout << "\nNO | Merek         |Model       | Tahun     |Harga\n";
-                cout << "----------------------------------------------------------\n";
+                cout << "\nNO | Merek        | Model        |Tahun      |Harga\n";
+                cout << "------------------------------------------------------------\n";
 
-                for (int i = 0; i < jumlah; i++) {
+                for (int i = 0; i < jumlah; i++){
                     cout << left
                     << setw(5) << i + 1
                     << setw(15) << merek[i]
@@ -62,12 +62,12 @@ int main() {
             break;
 
             case 3:
-            cout << "Keluar dari showroom.\n";
+            cout << "Keluar dari showroom\n";
             break;
 
             default:
-            cout << "Pilihan tidak valid.\n";
+            cout << "Pilihan tidak valid\n";
         }
-    } while (pilihan !=3);
+    }while (pilihan !=3);
     return 0;
 }
