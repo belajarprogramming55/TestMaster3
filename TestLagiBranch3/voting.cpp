8c656bb0a7fe989d0fd049d6b7349a6a96465053
OVAN <giovannimahendra55@gmail.com>
2026-01-06 13:21:14 +0700

    branch

--- TestLagiBranch3/voting.cpp
+++ TestLagiBranch3/voting.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
 
 int main() {
-    cout << "Hello Bitches";
+    map<string, int> pemilih;
+    int totalpemilih;
+
+    cout << "=== SISTEM VOTING MOBIL ===" << endl;
+    cout << "Masukan jumlah pemilih: ";
+    cin >> totalpemilih;
+    cin.ignore();
+
+    for (int i = 1; i <= totalpemilih; i++) {
+        string merek;
+        cout << "\nPemilih ke-" << i << ", pilih merek mobil: ";
+        getline(cin, merek);
+        pemilih[merek]++;
+    }
+    cout << "=== HASIL VOTING ===" << endl;
+    for (auto& pemilih : pemilih) {
+        cout << pemilih.first << ": " << pemilih.second << " suara" << endl;
+    }
     return 0;
 }
